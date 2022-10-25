d3fdc9461bdb08548ea03d4f329f48a62018caff
katemur <muravyevakate@gmail.com>
2022-10-25 21:33:38 +0300

    intepolation works correctly now

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import sqlite3
-n = input("N:")
-db= sqlite3.connect("tables.db")
-results = db.execute("SELECT alpha FROM b2 ORDER BY ABS(793 - np) LIMIT 2;")
-print(results)
+from cs50 import SQL
+def interpolation(n):
+  # accessing the database
+  db = SQL("sqlite:///tables.db")
+  #trying to find alpha without interpolation
+  result = db.execute("SELECT alpha FROM b2 WHERE np=?;", n)
+  #if not succesfull
+  if not result:
+    #getting two closest to input np values as 2 dicts
+    more, less = db.execute("SELECT np, alpha FROM b2 ORDER BY ABS(? - np) LIMIT 2;", n)
+    l_alpha = float(less.get("alpha"))
+    l_np = float(less.get("np"))
+    m_alpha = float(more.get("alpha"))
+    m_np = float(more.get("np"))
+    res_alpha = l_alpha + (n - l_np)/(m_np - l_np) * (m_alpha - l_alpha)
+  else:
+    res_alpha = result[0].get("alpha")
+  print (res_alpha)
+  
+def main():
+  n= int(input("N:"))
+  interpolation(n)
+
+main()
